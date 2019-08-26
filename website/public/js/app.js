7f5509fc546a667af860bfe7f753ff19473625b7
Entreprise KALAMAR <entreprise.kalamar@gmail.com>
2019-08-26 11:47:09 +0000

    feat: Modal preview for each platform

--- website/public/js/app.js
+++ website/public/js/app.js
@@ -1,3 +1,20 @@
 window.addEventListener('load', function () {
-    alert('Loaded')
+    var pElement = document.getElementsByClassName('platforms');
+    var $modal = $('#exampleModal');
+    Array.from(pElement).forEach((p) => {
+        // Native
+        p.addEventListener('click', function (ev) {
+            ev.preventDefault();
+            var jsPlatorm = JSON.parse(p.dataset['platform']);
+            // console.log(modal);
+            // show.bs.modal event
+            $modal.on('show.bs.modal', function (e) {
+                $('#exampleModalLabel').text(jsPlatorm['nom']);
+                $('#exampleModalBody').text(jsPlatorm['description']);
+            })
+            // console.log(p.querySelector('a'))
+            // console.log(p.dataset['platform']);
+            // alert(jsPlatorm['id'])large-platform-modal
+        });
+    });
 }, false);
